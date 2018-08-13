998cfbf7168285f330e4a62a851ca861065b60d4
Nasiha Ocasio <ocasin01@gmail.com>
2018-08-13 22:06:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ function happyHolidays(string) {return "Happy holidays!"}
 function happyHolidaysTo(name) {return "Happy holidays, " + name + "!" }
 happyHolidaysTo('you')
 function happyHolidayTo(holiday, name) {return "Happy " + holiday + ", " + name + "!"}
-function holidayCountdown(days, holiday) {return "It\'s " + days + " days until " + holiday + "!"}
+function holidayCountdown(holiday, days) {return "It\'s " + days + " days until " + holiday + "!"}
